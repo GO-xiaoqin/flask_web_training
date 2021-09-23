ec8b192753460836069253474847ee8ee827d5e1
GO-xiaoqin <59362515+GO-xiaoqin@users.noreply.github.com>
2021-09-23 17:46:50 +0800

    更改核心配置

--- config.py
+++ config.py
@@ -15,8 +15,8 @@ class Config:
     MAIL_SERVER = 'smtp.qq.com'
     MAIL_PORT = 587
     MAIL_USE_TLS = True
-    MAIL_USERNAME = os.environ.get('MAIL_USERNAME') or '55121236@qq.com'
-    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD') or 'fglaocdidxgmbhbd'
+    MAIL_USERNAME = os.environ.get('MAIL_USERNAME') or '**************'
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD') or '**************'
     FLASKY_MAIL_SUBJECT_PREFIX = '[Flasky]'
     FLASKY_MAIL_SENDER = '55121236@qq.com'
     FLASKY_ADMIN = '55121236@qq.com'
